26b36a2f7f22737fb0214d5388b0bf67310d0471
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-10-16 15:58:49 +0900

    🔖 v0.1.2

--- kanban/banner.go
+++ kanban/banner.go
@@ -1,10 +1,10 @@
 package kanban
 
 // Version ...
-var Version = "v0.1.1"
+var Version = "v0.1.2"
 
 // Banner ...
 var Banner = "* QQ + NanoBot + Golang\n" +
-	"* Version " + Version + " - 2023-10-16 13:41:39 +0800 CST\n" +
+	"* Version " + Version + " - 2023-10-16 15:58:35 +0800 CST\n" +
 	"* Copyright © 2023 - 2023 FloatTech. All Rights Reserved.\n" +
 	"* Project: FloatTech/NanoBot-Plugin"
