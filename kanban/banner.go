9892d654ef1356d50765de9453100535bccb4821
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-10-16 13:52:24 +0900

    fix help

--- kanban/banner.go
+++ kanban/banner.go
@@ -7,4 +7,4 @@ var Version = "v0.1.1"
 var Banner = "* QQ + NanoBot + Golang\n" +
 	"* Version " + Version + " - 2023-10-16 13:41:39 +0800 CST\n" +
 	"* Copyright © 2023 - 2023 FloatTech. All Rights Reserved.\n" +
-	"* Project: https://github.com/FloatTech/NanoBot-Plugin"
+	"* Project: FloatTech/NanoBot-Plugin"
--- kanban/gen/banner.go
+++ kanban/gen/banner.go
@@ -19,7 +19,7 @@ var Version = "%s"
 var Banner = "* QQ + NanoBot + Golang\n" +
 	"* Version " + Version + " - %s\n" +
 	"* Copyright © 2023 - %d FloatTech. All Rights Reserved.\n" +
-	"* Project: https://github.com/FloatTech/NanoBot-Plugin"
+	"* Project: FloatTech/NanoBot-Plugin"
 `
 
 const timeformat = `2006-01-02 15:04:05 +0800 CST`
