8273c9716a70ef65de56f29cd7988a97764e572b
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-12-02 03:25:11 +0900

    fix: qunyou

--- plugin/qunyou/main.go
+++ plugin/qunyou/main.go
@@ -19,18 +19,20 @@ func init() {
 	en.OnMessagePrefix("看看群友").Limit(ctxext.LimitByGroup).Handle(func(ctx *nano.Ctx) {
 		prompt := ctx.State["args"].(string)
 		sb := strings.Builder{}
+		errsb := strings.Builder{}
 		cmd := exec.Cmd{
 			Path:   "/usr/local/bin/llama2.run",
-			Args:   []string{"model.bin"},
+			Args:   []string{"/usr/local/bin/llama2.run", "model.bin"},
 			Dir:    "/usr/local/src/llama2.c",
 			Stdout: &sb,
+			Stderr: &errsb,
 		}
 		if prompt != "" {
 			cmd.Args = append(cmd.Args, "-i", prompt)
 		}
 		err := cmd.Run()
 		if err != nil {
-			ctx.SendChain(nano.Text("ERROR: ", err))
+			ctx.SendChain(nano.Text("ERROR: ", err, errsb.String()))
 			return
 		}
 		ctx.SendChain(nano.Text(sb.String()))
